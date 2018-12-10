04708348ffd022ee5b635ac3530dfcc394fb6cb9
Duo 'Erica' Xu <xdd.erica@gmail.com>
2018-12-10 16:39:36 -0500

    Add files via upload

--- Code/vcf2fasta_AltaiNean_Den_rmhetero_erica.py
+++ Code/vcf2fasta_AltaiNean_Den_rmhetero_erica.py
@@ -134,10 +134,23 @@ while n <= 1:    #phase3 2504, phase1 1092, n is the number of individuals, then
     haplotype2 = []
     dic_psn1_sorted = collections.OrderedDict(sorted(dic_psn1.items()))
     dic_psn2_sorted = collections.OrderedDict(sorted(dic_psn2.items()))
-    for p in dic_psn1_sorted:
-        haplotype1.append(dic_psn1_sorted[p])
+
+    locList = []
+    for eachLoc in dic_psn2.keys():
+        locList.append(int(eachLoc))
+
+    locList = sorted(locList)
+
+    for p in locList:
+        haplotype1.append(dic_psn1_sorted[str(p) + '_1'])
     for q in dic_psn2_sorted:
-        haplotype2.append(dic_psn2_sorted[q])
+        haplotype2.append(dic_psn2_sorted[str(q)])
+
+
+    # for p in dic_psn1_sorted:
+    #     haplotype1.append(dic_psn1_sorted[p])
+    # for q in dic_psn2_sorted:
+    #     haplotype2.append(dic_psn2_sorted[q])
     all_haplo[id[n+8] + '.1'] = ''.join(haplotype1)
     all_haplo[id[n+8] + '.2'] = ''.join(haplotype2)
     n = n + 1
